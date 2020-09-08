16a18efae2f8e7ca04053cee6e8df3397da8ea71
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2020-09-08 16:22:10 +0900

    Added DFA, NFA and DOT from regexp-scala

--- src/main/scala/Machines.scala
+++ src/main/scala/Machines.scala
@@ -398,3 +398,61 @@ class DFA[Q, A]
 
   def equiv(other: DFA[Q, A]): Boolean = (this symdiff other).isEmpty
 }
+
+class NFA[Q, A]
+(
+  val states: Set[Q],
+  val alpha: Set[A],
+  t: Map[(Q, Option[A]), Set[Q]], // εをNoneで表現
+  val q0: Q,
+  val finalStates: Set[Q]
+) {
+
+  val transition: Map[(Q, Option[A]), Set[Q]] = t.withDefaultValue(Set())
+  // キーに対して値が定義されていないときに空集合を返す
+
+  def eclosure(aQs: Set[Q]): Set[Q] = {
+    var qs = Set[Q]()
+    var newQs = aQs
+    while (newQs != qs) {
+      qs = newQs
+      for (q <- qs) newQs = newQs ++ transition((q, None))
+    }
+    qs
+  }
+
+  def transSet(qs: Set[Q], w: List[A]): Set[Q] =
+    w match {
+      case Nil => eclosure(qs)
+      case a :: w =>
+        transSet(eclosure(qs).flatMap(q => transition((q, Some(a)))), w)
+    }
+
+  def trans(q: Q, w: List[A]): Set[Q] = transSet(Set(q), w)
+
+  def accept(w: List[A]): Boolean =
+    (trans(q0, w) & finalStates).nonEmpty
+
+  def toDFA: DFA[Set[Q], A] = {
+    val q0DFA = eclosure(Set(q0))
+    var statesDFA = Set(q0DFA)
+    var u = List(q0DFA) // リストをスタックとして使用
+    var transitionDFA = Map[(Set[Q], A), Set[Q]]()
+
+    while (u.nonEmpty) {
+      val s = u.head
+      u = u.tail
+      for (a <- alpha) {
+        val t = eclosure(s.flatMap(q => transition((q, Some(a)))))
+        transitionDFA += (s, a) -> t
+        if (!statesDFA.contains(t)) {
+          u = t :: u
+          statesDFA += t
+        }
+      }
+    }
+    val finalStatesDFA = statesDFA.filter(qs => (qs & finalStates).nonEmpty)
+
+    new DFA(statesDFA, alpha, transitionDFA, q0DFA, finalStatesDFA)
+  }
+}
