598a78ba95c24bc4eda5157a2c2498c0e9a7dbba
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2021-07-26 22:58:20 +0900

    Revised
    - changed one example constraint so that even the latest OSTRICH
      cannot solve it (sect. 2)
    - changed notation of variable updates from m to \alpha (sect. 3)
    - tried to explain copylessness is not preserved for deterministic
      cases, but for preimage strategy it actually is

--- bench_jssst.fish
+++ bench_jssst.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/fish
-for file in constraints/bench/{indexof,indexof_sat,deleteall,substr_equiv,inverse_sat,inverse_unsat}
+for file in constraints/bench/{indexof2_sat,indexof2_unsat,indexof,indexof_sat,deleteall,substr_equiv,inverse_sat,inverse_unsat}
     echo ";; $file ;;"
     time java -jar target/scala-2.13/expresso-assembly-0.3.0-SNAPSHOT.jar $file.smt2 thesis | python3 summary_jssst.py
 end
