ad4d089598af56a05a1a6c5fd364c5bb5f665213
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2022-05-15 15:11:12 +0900

    add some command-line options and use `decline` for parsing them all

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,103 @@
 package com.github.kmn4.expresso
 
+import cats.data.Validated
+import cats.data.ValidatedNel
+import ch.qos.logback.{classic => logback}
+import com.github.kmn4.expresso.strategy.JSSST2021Strategy
 import com.github.kmn4.expresso.strategy.PreImageStrategy
+import com.microsoft.z3.Version
+import com.monovore.decline.CommandApp
+import com.typesafe.scalalogging.Logger
 
 import java.nio.file.FileSystems
-import com.typesafe.scalalogging.Logger
-import com.github.kmn4.expresso.strategy.JSSST2021Strategy
+import java.nio.file.Path
 
-// command line arguments:
-// - 0 : constraint file name (.smt2 file)
-// - 1 : solving strategy, either 'preimage' or 'jssst' (defaults to 'jssst')
-object Main extends App {
-  val fname = args(0)
-  val logger = Logger(s"bench.${fname.split('/').last.split('.').dropRight(1).mkString}")
-  val path = FileSystems.getDefault().getPath(fname)
-  val lexer = new smtlib.lexer.Lexer(new java.io.FileReader(path.toFile()))
-  val parser = new smtlib.parser.Parser(lexer)
-  val script = parser.parseScript
-  val checker = args.applyOrElse(1, (_: Int) => "jssst") match {
-    case "preimage" => new PreImageStrategy(logger)
-    case "jssst"   => new JSSST2021Strategy(logger)
-    case s          => throw new Exception(s"Invalid strategy: ${s}\nPossible values are: preimage, jssst")
+object Main
+    extends CommandApp(
+      name = "expresso",
+      header = "Solve straight-line string constraints.",
+      version = s"SNAPSHOT (with ${Version.getFullVersion()})",
+      main = Options.opts map { arguments =>
+        val path   = arguments.constraintFile
+        val file   = path.toFile()
+        val logger = Logger(s"bench.${file.getName().split('.').dropRight(1).mkString}")
+        arguments.loggingLevel map Options.loggingLevels foreach
+          logger.underlying.asInstanceOf[logback.Logger].setLevel
+        val parser   = new smtlib.parser.Parser(new smtlib.lexer.Lexer(new java.io.FileReader(file)))
+        val script   = parser.parseScript                 // might throw an exception
+        val checker  = Options.strategies(arguments.strategyName)(logger)
+        val alphabet = arguments.alphabet getOrElse Set() // alpahbet to be added
+
+        logger.trace(s"checker=$checker")
+        logger.trace(s"logger=$logger")
+        logger.trace(s"alphabet=$alphabet")
+
+        new Solver(
+          checker = checker,
+          print = true,
+          logger = logger,
+          alphabet = alphabet
+        ).executeScript(script)
+      }
+    )
+
+private final case class Arguments(
+    constraintFile: Path,
+    strategyName: String,
+    alphabet: Option[Set[Char]],
+    loggingLevel: Option[String],
+)
+
+private object Options {
+  import com.monovore.decline._
+  import cats.implicits._
+
+  def opts: Opts[Arguments] = (constraintFile, strategy, alphabet, loggingLevel) mapN Arguments.apply
+
+  val strategies = Map(
+    "jssst"    -> (logger => new JSSST2021Strategy(logger)),
+    "preimage" -> (logger => new PreImageStrategy(logger)),
+  )
+
+  val loggingLevels = scala.collection.immutable.SeqMap(
+    "all"   -> logback.Level.TRACE,
+    "trace" -> logback.Level.TRACE,
+    "debug" -> logback.Level.DEBUG,
+    "info"  -> logback.Level.INFO,
+    "warn"  -> logback.Level.WARN,
+    "error" -> logback.Level.ERROR,
+    "off"   -> logback.Level.OFF,
+  )
+
+  val constraintFile = Opts
+    .argument[Path](metavar = "constraint file")
+    .validate("<constraint file> should exist")(_.toFile().exists())
+  val strategy = Opts
+    .argument[String]("strategy")
+    .withDefault("jssst")
+    .validate("<strategy> must be either \"preimage\" or \"jssst\"")(strategies.keySet.contains)
+  val loggingLevel = {
+    val keys   = loggingLevels.keys
+    val msg    = s"one of ${keys.init.mkString(", ")}, or ${keys.last}"
+    val keySet = keys.toSet
+    Opts
+      .option[String]("logging-level", help = msg)
+      .validate(s"<logging-level> must be $msg")(keySet.contains)
+      .orNone
   }
+  val alphabet = Opts
+    .option[Set[Char]](
+      "alphabet",
+      help = "alphabet to be considered additionally to those contained in a given constraint"
+    )(new Argument[Set[Char]] {
+      def read(string: String): ValidatedNel[String, Set[Char]] = string.split("-").toSeq match {
+        case Seq(from, to) if from.length() == 1 && to.length() == 1 =>
+          Validated.valid((from(0) to to(0)).toSet[Char])
+        case _ => Validated.invalidNel("argument to --alphabet must be in the form <char>-<char>")
+      }
 
-  // alpahbet to be added
-  val alphabet = ('a' to 'c').toSet
-//  val alphabet = ('!' to '~').toSet
-//  val alphabet = (0 to 127).toSet
+      def defaultMetavar: String = "alphabet"
+    })
+    .orNone
 
-  new Solver(
-    checker = checker,
-    print = true,
-    logger = logger,
-    alphabet = alphabet
-  ).executeScript(script)
 }
